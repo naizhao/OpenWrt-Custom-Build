a7f081a0fd287f1473bc82bb825443b9663737d7
Sam Wu <me@naizhao.com>
2021-01-29 14:12:52 +0800

    updated script

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -10,7 +10,7 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-sed -i 's/coolsnowwolf\/packages/naizhao\/packages/g' feeds.conf.default
+#sed -i 's/coolsnowwolf\/packages/naizhao\/packages/g' feeds.conf.default
 
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
