a40582578ef3d7cf9880f40c8ed477939af6ee82
Sam Wu <me@naizhao.com>
2021-04-09 11:42:34 +0800

    changed passwall repo url

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -21,7 +21,8 @@ pushd package/community
 git clone --depth=1 https://github.com/Lienol/openwrt-package
 
 # Add luci-app-passwall
-git clone --depth=1 https://github.com/naizhao/openwrt-passwall
+#git clone --depth=1 https://github.com/naizhao/openwrt-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
 
 # Add luci-app-vssr <M>
 git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb.git
